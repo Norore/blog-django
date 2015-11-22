1efcbd61f7be5ea056d44320e5697eaeeaf81351
Norore <nolwenn.lavielle@gmail.com>
2015-11-22 23:14:44 +0100

    Improve home view

--- blog/Blog/views.py
+++ blog/Blog/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from models import *
 
 # Create your views here.
 def index(request):
-    return render(request, "home.html")
+    links = Link.objects.all()
+    cats = Categorie.objects.all().order_by('id')
+    articles = Article.objects.filter(published=True).order_by('-creation_date')[:10]
+    pages = Page.objects.all().order_by('title')
+    context = {'links': links,
+               'cats': cats,
+               'articles': articles,
+               'pages': pages}
+    return render(request, "home.html", context)
