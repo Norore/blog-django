1420f66feabbaa521d335850fdaf85dac86dbe95
Norore <nolwenn.lavielle@gmail.com>
2015-11-24 20:36:14 +0100

    Show tags per article

--- blog/Blog/views.py
+++ blog/Blog/views.py
@@ -4,11 +4,13 @@ from models import *
 # Create your views here.
 def index(request):
     links = Link.objects.all()
-    cats = Categorie.objects.all().order_by('id')
+    cats = Categorie.objects.all().order_by('name')
     articles = Article.objects.filter(published=True).order_by('-creation_date')[:10]
+    tags = Tags.objects.order_by('article__id').order_by('id')
     pages = Page.objects.all().order_by('title')
     context = {'links': links,
                'cats': cats,
                'articles': articles,
-               'pages': pages}
+               'pages': pages,
+               'tags': tags}
     return render(request, "home.html", context)
