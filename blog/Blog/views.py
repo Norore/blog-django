b849596bdfbcb6324753da8a4579ff244aaa6412
Norore <nolwenn.lavielle@gmail.com>
2015-12-11 22:54:09 +0100

    Improve views, base, home and check if article/category/page exists

--- blog/Blog/views.py
+++ blog/Blog/views.py
@@ -1,13 +1,14 @@
 from django import get_version
 from django.shortcuts import render
 from models import *
+from django.db.models import Count
 
 # Create your views here.
 def base():
     links = Link.objects.all()
-    cats = Categorie.objects.all().order_by('name')
-    pages = Page.objects.all().order_by('title')
-    archives = Article.objects.all().filter(published=True)
+    cats = Categorie.objects.filter(article__published=True).annotate(nb_articles=Count('article__id')).order_by('name')
+    pages = Page.objects.filter(published=True).order_by('title')
+    archives = Article.objects.filter(published=True)
 
     context = { 'version': get_version(),
                 'links': links,
@@ -19,14 +20,20 @@ def base():
 
 def index(request):
     context = base()
-    articles = Article.objects.filter(published=True).order_by('-creation_date')[:10]
+
+    articles = Article.objects.filter(published=True).annotate(nb_comments=Count('comment__id')).order_by('-creation_date')[:10]
     comments = Comment.objects.filter(published=True, article__published=True).order_by('-article__creation_date')[:10].values()
+    
     context['articles'] = articles
     context['comments'] = comments
+    
     return render(request, "home.html", context)
 
 def show_page(request, page):
-    page = Page.objects.get(id=page)
+    try:
+        page = Page.objects.get(id=page)
+    except:
+        page = {}
 
     context = base()
     context['page'] = page
@@ -34,8 +41,12 @@ def show_page(request, page):
     return render(request, "show_page.html", context)
 
 def show_categorie(request, cat):
-    categorie = Categorie.objects.get(id=cat)
-    articles = Article.objects.filter(categorie=cat, published=True).order_by('-creation_date')
+    try:
+        categorie = Categorie.objects.get(id=cat)
+        articles = Article.objects.filter(categorie=cat, published=True).order_by('-creation_date')
+    except:
+        categorie = {}
+        articles = {}
 
     context = base()
     context['categorie'] = categorie
@@ -44,8 +55,12 @@ def show_categorie(request, cat):
     return render(request, "show_categorie.html", context)
 
 def show_article(request, art):
-    article = Article.objects.get(id=art)
-    comments = Comment.objects.filter(article__id=art)
+    try:
+        article = Article.objects.get(id=art)
+        comments = Comment.objects.filter(article__id=art, published=True)
+    except:
+        article = {}
+        comments = {}
 
     context = base()
     context['article'] = article
